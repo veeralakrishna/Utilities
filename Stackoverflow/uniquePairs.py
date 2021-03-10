395040a32a90e2d7695eab511a482f27a5551024
Veerala Hari Krishna <veeralakrishna@gmail.com>
2021-03-10 11:58:43 +0530

    Update uniquePairs.py

--- Stackoverflow/uniquePairs.py
+++ Stackoverflow/uniquePairs.py
@@ -4,3 +4,9 @@ def uniqueCombinations(numeric_col):
     s = set(l)
     # print('actual', len(l), l)
     return list(s)
+
+
+import itertools
+
+def unique_combinations(list_elements):
+    return list(itertools.combinations(set(list_elements), 2))
